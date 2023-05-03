d3e39790f080c27e26d3485ff979f3b8344d497d
mikemont09 <130870299+mikemont09@users.noreply.github.com>
2023-05-03 14:19:31 -0400

    Homework #22

---
+++ src/test/java/pages/LoginPage.java
@@ -0,0 +1,41 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class LoginPage extends BasePage {
+
+    //locators
+    By submitButtonLocator = By.cssSelector("button[type='submit']");
+    By emailField = By.cssSelector("input[type='email']");
+    By passwordField = By.cssSelector("input[type='password']");
+
+    //constructor
+    public LoginPage( WebDriver givenDriver) {
+        super(givenDriver);
+    }
+    //page methods
+    public void clickSubmitBtn() {
+        driver.findElement(submitButtonLocator).click();
+    }
+    public void provideEmail(String email) {
+        WebElement emailElement = driver.findElement(emailField);
+        emailElement.click();
+        emailElement.sendKeys(email);
+    }
+    public void providePassword(String password) {
+        WebElement passwordElement = driver.findElement(passwordField);
+        passwordElement.click();
+        passwordElement.sendKeys(password);
+    }
+    public void login(){
+        provideEmail("monty133@gmail.com");
+        providePassword("te$t$tudent");
+        clickSubmitBtn();
+    }
+
+}
